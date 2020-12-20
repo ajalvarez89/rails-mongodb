69fb67079704a07402e8e14d79003950c56b62a3
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-12-20 17:16:47 -0500

    instalation and configuration of rspec, capybara, database_cleaner

---
+++ spec/rails_helper.rb
@@ -0,0 +1,37 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+require 'factory_bot_rails'
+require 'database_cleaner'
+require 'capybara/rails'
+
+RSpec.configure do |config|
+  # Remove this line to enable support for ActiveRecord
+  config.use_active_record = false
+  config.use_transactional_fixtures = false
+  config.include FactoryBot::Systax::Methods
+
+  #helpers for sessions with devise
+  config.include Warden::Test::Helpers
+  config.include Devise::Test::ControllersHelpers, type: 'controller'
+  config.before(:suite) do
+    DatabaseCleaner.orm = 'mongoid'
+    DatabaseCleaner.clean
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
