ea0868e188abd2a0ec0b2cfbd67868932a8e2877
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-12-20 13:46:05 -0500

    finish the migration to mongodb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,22 +1,16 @@
-# == Schema Information
-#
-# Table name: tasks
-#
-#  id          :bigint           not null, primary key
-#  name        :string
-#  description :text
-#  due_date    :date
-#  category_id :bigint           not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  owner_id    :bigint           not null
-#  code        :string
-#
-class Task < ApplicationRecord
+class Task
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+  field :description, type: String
+  field :due_date, type: Date
+  field :code, type: String
+
   belongs_to :category
   belongs_to :owner, class_name: 'User'
   has_many :participating_users, class_name: 'Participant'
-  has_many :participants, through: :participating_users, source: :user
+  # has_many :participants, through: :participating_users, source: :user
   has_many :notes
 
   validates :participating_users, presence: true
@@ -30,6 +24,10 @@ class Task < ApplicationRecord
 
   accepts_nested_attributes_for :participating_users, allow_destroy: true
 
+  def participants
+    participating_users.includes(:user).map(&:user)
+  end
+
   def due_date_validity
     return if due_date.blank?
     return if due_date > Date.today
@@ -41,6 +39,7 @@ class Task < ApplicationRecord
   end
 
   def send_email
+    return
     return unless Rails.env.development?
     (participants + [owner]).each do |user|
       ParticipantMailer.with(user: user, task: self).new_task_email.deliver!
